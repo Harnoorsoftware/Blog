e626288c4c8376dbafa9fcf65351059b44e5f039
Harnoorsoftware <harnoorsoftware@gmail.com>
2025-05-26 19:25:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="My HTML Page")
 with open("index .html", "r", encoding="utf-8") as f:
     html_content = f.read()
 
-st.image("template/image.jpg", caption="My Image", use_column_width=True)
+st.image("img.png", caption="My Image", use_column_width=True)
 
 # Display it in the Streamlit app
 st.components.v1.html(html_content, height=600, scrolling=True)
