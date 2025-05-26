9192259b7a1703c74cebad1929ce91be320e5e4d
Harnoorsoftware <harnoorsoftware@gmail.com>
2025-05-26 21:54:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(page_title="My HTML Page")
 
 # Load the HTML file
-with open("index.html", "r", encoding="utf-8") as f:
+with open("index .html", "r", encoding="utf-8") as f:
     html_content = f.read()
 
 # Display it in the Streamlit app
